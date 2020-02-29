5ece2f502a8166260c81240e9f662b1c4c564fd4
Evan Tatarka <evan@tatarka.me>
2020-02-29 01:38:08 -0500

    Bare bones

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,21 @@
 use std::{fs, io};
-use std::path::Path;
+use std::borrow::Cow;
+use std::ffi::{OsStr, OsString};
+use std::path::{Path, PathBuf};
 
 use clap::Clap;
-use std::ffi::OsStr;
-use term_grid::{Grid, GridOptions, Filling, Direction};
+use nom::branch::alt;
+use nom::bytes::complete::{is_a, is_not, tag, take_until, take_while};
+use nom::character::complete::anychar;
+use nom::combinator::{all_consuming, map, not, recognize};
+use nom::error::ParseError;
+use nom::InputLength;
+use nom::multi::many0;
+use nom::sequence::{delimited, pair, separated_pair, terminated, tuple};
+use rand::prelude::{IteratorRandom, SliceRandom};
+use term_grid::{Direction, Filling, Grid, GridOptions};
+use textwrap::{fill, wrap};
+use unicode_width::UnicodeWidthStr;
 
 #[derive(Clap)]
 #[clap(version = "1.0", author = "Pixel Light")]
@@ -11,26 +23,140 @@ struct Opts {
     /// List pony names.
     #[clap(short, long, exclusive = true)]
     list: bool,
+    #[clap(short, long, exclusive = true)]
+    quote: bool,
 }
 
 fn main() -> io::Result<()> {
     let opts = Opts::parse();
 
+    let pony_dir = Path::new("/usr/share/ponysay/ponies/");
+    let pony_quote_dir = Path::new("/usr/share/ponysay/quotes");
+
     if opts.list {
-        let pony_dir = Path::new("/usr/share/ponysay/ponies/");
         print_pony_list(pony_dir)?;
-    } else {}
+    } else if opts.quote {
+        print_pony_quote(pony_dir, pony_quote_dir)?;
+    }
 
     Ok(())
 }
 
+fn print_pony_quote(pony_dir: &Path, pony_quote_dir: &Path) -> io::Result<()> {
+    let rng = &mut rand::thread_rng();
+
+    let pony_quote_paths: Vec<_> = paths(pony_quote_dir)?.collect();
+    let pony_quote_path = pony_quote_paths.choose(rng).unwrap();
+
+    let pony_names = pony_quote_path.file_stem().unwrap().to_string_lossy();
+    let pony_name = pony_names
+        .split("+")
+        .choose(rng).unwrap();
+
+    let pony_path = paths(pony_dir)?
+        .find(|path| to_pony_name(path).map(|n| n == pony_name).unwrap_or(false))
+        .unwrap();
+    let pony_quote = fs::read_to_string(&pony_quote_path)?;
+
+    let pony = fs::read_to_string(pony_path)?;
+    let (_, (metadata, pony)) = parse_pony(create_balloon(&pony_quote), &pony).unwrap();
+
+    println!("{}", pony);
+
+    Ok(())
+}
+
+fn parse_pony(quote: String, pony: &str) -> nom::IResult<&str, (Vec<(&str, &str)>, String)> {
+    pair(parse_metadata, parse_pony_body(quote))(pony)
+}
+
+fn parse_balloon<'a>(quote: String) -> impl Fn(&'a str) -> nom::IResult<&'a str, String> {
+    move |pony| {
+        let (pony, _) = delimited(tag("$"), is_not("$"), tag("$"))(pony)?;
+        Ok((pony, quote.clone()))
+    }
+}
+
+fn parse_metadata(pony: &str) -> nom::IResult<&str, Vec<(&str, &str)>> {
+    delimited(tag("$$$\n"), many0(parse_metadata_line), pair(is_not("$"), tag("$$$\n")))(pony)
+}
+
+fn parse_metadata_line(pony: &str) -> nom::IResult<&str, (&str, &str)> {
+    terminated(separated_pair(is_not(":"), tag(":"), is_not("\n")), tag("\n"))(pony)
+}
+
+fn parse_pony_body<'a>(quote: String) -> impl Fn(&'a str) -> nom::IResult<&'a str, String> {
+    move |pony| {
+        let (pony, v): (_, Vec<Cow<str>>) = many0(alt((
+            map(parse_stem, |s| s.into()),
+            map(parse_balloon(quote.clone()), |s| s.into()),
+            map(is_not("$"), |s: &str| s.into())
+        )))(pony)?;
+        Ok((pony, v.join("")))
+    }
+}
+
+fn parse_stem(pony: &str) -> nom::IResult<&str, String> {
+    map(delimited(tag("$"), is_a("\\/"), tag("$")), |stem| format!(" {} ", stem))(pony)
+}
+
+fn create_balloon(quote: &str) -> String {
+    let wrapped_text = wrap(quote, 65);
+    let width = wrapped_text.iter()
+        .map(|t| t.width())
+        .fold(0, usize::max);
+
+    let mut output = String::new();
+    output.push(' ');
+    for _ in 0..(width + 2) {
+        output.push('_');
+    }
+    output.push(' ');
+    output.push('\n');
+    if wrapped_text.len() == 1 {
+        output.push_str(&format!("< {} >\n", wrapped_text[0].trim_end()));
+    } else {
+        for i in 0..wrapped_text.len() {
+            let line = wrapped_text[i].trim_end();
+            let end_padding = width - line.width();
+            let (start_char, end_char) = if i == 0 {
+                ('/', '\\')
+            } else if i == wrapped_text.len() - 1 {
+                ('\\', '/')
+            } else {
+                ('|', '|')
+            };
+            output.push(start_char);
+            output.push(' ');
+            output.push_str(line);
+            for _ in 0..(end_padding + 1) {
+                output.push(' ');
+            }
+            output.push(end_char);
+            output.push('\n');
+        }
+    }
+    output.push(' ');
+    for _ in 0..(width + 2) {
+        output.push('-');
+    }
+    output.push(' ');
+    return output;
+}
+
+fn paths(dir: &Path) -> io::Result<impl Iterator<Item=PathBuf>> {
+    Ok(fs::read_dir(dir)?.into_iter()
+        .filter_map(|entry| entry.ok().map(|e| e.path())))
+}
+
 fn print_pony_list(pony_dir: &Path) -> io::Result<()> {
-    let mut pony_names = fs::read_dir(pony_dir)?.into_iter()
-        .filter_map(|entry| entry.ok().and_then(|e| to_pony_name(&e.path())))
+    let mut pony_names = paths(pony_dir)?
+        .filter_map(|path| to_pony_name(&path))
         .collect::<Vec<_>>();
     pony_names.sort();
 
-    if let Some((term_width, _)) = term_size::dimensions() {
+    if let Some((term_width, _)) = term_size::dimensions_stdout() {
+        println!("ponies located in {}", pony_dir.display());
         print_columns(pony_names, term_width);
     } else {
         print_single_column(pony_names);
@@ -41,8 +167,8 @@ fn print_pony_list(pony_dir: &Path) -> io::Result<()> {
 
 fn print_columns(pony_names: Vec<String>, size: usize) {
     let mut grid = Grid::new(GridOptions {
-        filling:     Filling::Spaces(2),
-        direction:   Direction::TopToBottom,
+        filling: Filling::Spaces(2),
+        direction: Direction::TopToBottom,
     });
 
     for pony_name in pony_names {
@@ -67,5 +193,13 @@ fn to_pony_name(path: &Path) -> Option<String> {
         Some(path.file_stem()?.to_str()?.to_owned())
     } else {
         None
+    }
+}
+
+pub fn eof<I: Copy + InputLength, E: ParseError<I>>(input: I) -> nom::IResult<I, I, E> {
+    if input.input_len() == 0 {
+        Ok((input, input))
+    } else {
+        Err(nom::Err::Error(E::from_error_kind(input, nom::error::ErrorKind::Eof)))
     }
 }
--- tests/cli.rs
+++ tests/cli.rs
@@ -1,13 +1,26 @@
 use std::process::Command;
-use assert_cmd::prelude::*;
-use predicates::prelude::*;
+use assert_cmd::cargo::CommandCargoExt;
 
 #[test]
 fn pony_list() -> Result<(), Box<dyn std::error::Error>> {
-    let mut cmd = Command::cargo_bin(env!("CARGO_PKG_NAME"))?;
-    cmd.arg("-l");
-
-    let mut python_ponysay = Command::new("ponysay");
+    let mut ponysay = Command::cargo_bin(env!("CARGO_PKG_NAME"))?;
+    ponysay.arg("-l");
+    let mut python_ponysay = python_ponysay();
     python_ponysay.arg("-l");
-    
+
+    assert_eq!(ponysay.output()?, python_ponysay.output()?);
+
+    Ok(())
+}
+
+fn stty() -> Command {
+    let mut cmd = Command::new("stty");
+    cmd.arg("size");
+    return cmd;
+}
+
+fn python_ponysay() -> Command {
+    let mut python_ponysay = Command::new("python");
+    python_ponysay.arg("python-ponysay/src/__main__.py");
+    return python_ponysay;
 }
